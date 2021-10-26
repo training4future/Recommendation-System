30d76aad3c329ad8d13ac53edc213fe3bb80fcf2
Rubén Correoso <rubencc.pro@gmail.com>
2021-10-26 08:07:53 +0200

    final before send solution

--- Daimler_technical_test.py
+++ Daimler_technical_test.py
@@ -57,31 +57,45 @@ class RequestedNumber:
 
 
 # Recommendation class
-# Score 
+# Score user's sku code simillarities
 class Recommendation:
     def __init__(self, inserted_sku):
+        # User sku code row
         self.index_sku = int(inserted_sku[4:])-1
         
     def get_recommendations(self, df_transposed, len=10):
-        self.origin_sku = df_transposed.iloc[[self.index_sku]]
+        # Get len=10 recommendations If exists
         self.len=len
+        # Init score and score_tmp and columns for score calculation
         columns = df_transposed.columns
         df_transposed['score_tmp']=0
         df_transposed['score']=0
 
         for column in columns:
+            # For each column "att-a-[number]" lambda function aplly and If 
+            # target value is found score is increased, target value is the
+            # column value of the user's selected sku code.
+            # At the end of the for we will have a column 'score' with the
+            # number of simillarities between the row and the user's selected
+            # code
             target_value = df_transposed.iloc[self.index_sku][column]
             df_transposed['score_tmp'] = df_transposed[column].apply(lambda x: 1 if x==target_value else 0)
             df_transposed['score']=df_transposed['score']+df_transposed['score_tmp']
         
+        # Scored DF is sorted in descending mode depending of the score and
+        # truncated to len+1 size (11) being first element the user's selected
+        # code
         self.df_scored = df_transposed.sort_values(by='score', ascending=False).drop(['score_tmp'], axis=1).head(self.len+1)
 
     def print_recommendations(self):
-        print("Solution before formating:")
-        print(self.df_scored, "\n")
+        # Recommendations are printed row by rowin json format like the example
+        print("Recommendation before formating:")
+        print("\n", self.df_scored, "\n")
         pd.set_option("display.max_rows", None, "display.max_columns", None)
 
         df_final = self.df_scored.drop('score', axis=1)
+
+        print("Recommendation:")
 
         for i in df_final.index:
             print(df_final.loc[[i]].to_json(orient="index"))
@@ -93,52 +107,52 @@ class Recommendation:
 def main():
     try:
         # Begin dataset read process
-        print_log("[Info] Starting", "read")
+        print_log(log_info_ini, "read")
         
         # Create file object and read json dataset
         inputFileObject = InputFile(inputfile)
         df_read = inputFileObject.read_file()
    
         # End dataset read process
-        print_log("[Info] Ending", "read")
+        print_log(log_info_end, "read")
            
         # Begin dataset validation process
-        print_log("[Info] Starting", "validation")
+        print_log(log_info_ini, "validation")
 
-        # Transpose and validate dataset
+        # Transpose and validate dataset to have sku-code as row index
+        # and att-a... as columns
         df_transposed = df_read.T
         inputFileObject.validate_dataset(df_transposed)
         
         # End dataset validation process
-        print_log("[Info] Ending", "validation")
+        print_log(log_info_end, "validation")
         
         # Begin user request and validation process
-        print_log("[Info] Starting", "user request")
+        print_log(log_info_ini, "user request")
         
         # Request user sku code and validate it
         requestedSkuObject = RequestedNumber()
         requestedSkuObject.validate_sku(df_read)
         
         # End user request and validation process
-        print_log("[Info] Ending", "user request")
+        print_log(log_info_end, "user request")
         
         # Begin get recommendation process
-        print_log("[Info] Starting", "get recommendations")
+        print_log(log_info_ini, "get recommendations")
     
-        # Score similar sku codes and return 10 with higher score in descending
+        # Score similar sku codes and print 10 with higher score in descending
         # order
         recommendationObject = Recommendation(requestedSkuObject.inserted_sku)
         recommendationObject.get_recommendations(df_transposed)
         recommendationObject.print_recommendations()
         
         # End get recommendation process
-        print_log("[Info] Ending", "get recomendations")
+        print_log(log_info_end, "get recomendations")
     
     except Exception as e:
         print("[Error] "+str(e)) 
     
     
-
 # This function return current time
 def calculate_time():
     now = datetime.now()
@@ -151,10 +165,15 @@ def print_log(prefix, process):
 
     
 if __name__ == "__main__":
+    # Input path of the dataset
     inputfile="Daimler-test-data.json"
+    # Log function
+    log_info_ini="[Info] Starting"
+    log_info_end="[Info] Ending"
 
-    print_log("[Info] Starting", "main")
+    print_log(log_info_ini, "main")
     
+    # Main function invocation
     main()
     
-    print_log("[Info] Ending", "main")
+    print_log(log_info_end, "main")
